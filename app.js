b9e80d970c3d97db180436134b97a6f6b29b0883
saleh <sjaffal3@gmail.com>
2021-03-25 23:52:48 +0400

    completed most frontpage and skillset content + styling

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const App = {};
 
+App.switch = document.querySelector(".theme");
+
+App.darkmode = () => {
+
+  App.switch.addEventListener("click", function() {
+
+    document.querySelector( "body" ).classList.toggle( "dark" );
+    localStorage.setItem('dark',document.querySelector("body").classList.contains('dark'));    
+});
+}
+
+App.init = () => {
+
+  if (localStorage.getItem('dark') === 'true') {
+    document.querySelector( "body" ).classList.add( "dark" );
+  }
+
+  App.darkmode();
+  
+}
+
+window.addEventListener('load', (event) => {
+  AOS.init();
+  App.init();
+});
