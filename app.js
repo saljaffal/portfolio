551824d4c4a4080cfa23accfd2ca56b498873914
saleh <sjaffal3@gmail.com>
2021-03-26 20:31:12 +0400

    finished css for most elements, removed contacts for now will add later

--- app.js
+++ app.js
@@ -1,23 +1,78 @@
 const App = {};
 
-App.switch = document.querySelector(".theme");
+App.switch = document.querySelector('.theme');
+App.bodyClass = document.querySelector( 'body' ).classList; 
 
-App.darkmode = () => {
+App.darkMode = () => {
 
-  App.switch.addEventListener("click", function() {
+  // const illustration = document.querySelector('.illustration');
 
-    document.querySelector( "body" ).classList.toggle( "dark" );
-    localStorage.setItem('dark',document.querySelector("body").classList.contains('dark'));    
+  App.switch.addEventListener('click', function() {
+
+    App.bodyClass.toggle( 'dark' );
+    localStorage.setItem('dark',App.bodyClass.contains('dark'));    
 });
+}
+
+
+App.scrollUp = () => {
+  let scrollToTopBtn = document.querySelector('.scrollToTopBtn')
+  let rootElement = document.documentElement
+  let TOGGLE_RATIO = 0.80
+
+  function handleScroll() {
+    // do something on scroll
+    let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if ((rootElement.scrollTop / scrollTotal) > TOGGLE_RATIO) {
+      //show button
+      scrollToTopBtn.classList.add('showBtn')
+    } else {
+      //hide button
+      scrollToTopBtn.classList.remove('showBtn')
+    }
+  }
+
+  function scrollToTop() {
+    //scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+  scrollToTopBtn.addEventListener('click', scrollToTop)
+  document.addEventListener('scroll', handleScroll)
+}
+
+App.smoothScroll = () => {
+  const links = document.querySelectorAll('nav ul a');
+ 
+  for (const link of links) {
+    link.addEventListener('click', clickHandler);
+  }
+   
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const offsetTop = document.querySelector(href).offsetTop;
+   
+    scroll({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  }
 }
 
 App.init = () => {
 
   if (localStorage.getItem('dark') === 'true') {
-    document.querySelector( "body" ).classList.add( "dark" );
+    App.bodyClass.add( 'dark' );
   }
 
-  App.darkmode();
+  App.darkMode();
+  // App.typeEffect();
+  App.scrollUp();
+  App.smoothScroll();
+  console.log(`My Resume https://cutt.ly/LxGt5SG`);
   
 }
 
